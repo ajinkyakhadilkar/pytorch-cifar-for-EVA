c512d6b190fba1255ef5e6089ae9bb7a3cfdf09d
Ajinkya Khadilkar <ajinkyakhadilkar1997@gmail.com>
2023-02-11 01:32:59 +0530

    Fix issues with importing

--- main.py
+++ main.py
@@ -9,21 +9,22 @@ import torchvision
 import torchvision.transforms as transforms
 
 import os
-import argparse
 
-from ..models import *
-from utils import progress_bar
+from .models import resnet
+from .utils import progress_bar
 
-net = ResNet18()
+net = resnet.ResNet18()
 
 def set_net(net):
   net = this.net
 
+'''
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
 args = parser.parse_args()
+'''
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
@@ -78,6 +79,7 @@ if device == 'cuda':
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
 
+'''
 if args.resume:
     # Load checkpoint.
     print('==> Resuming from checkpoint..')
@@ -86,9 +88,9 @@ if args.resume:
     net.load_state_dict(checkpoint['net'])
     best_acc = checkpoint['acc']
     start_epoch = checkpoint['epoch']
-
+'''
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr,
+optimizer = optim.SGD(net.parameters(), lr=0.001,
                       momentum=0.9, weight_decay=5e-4)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
